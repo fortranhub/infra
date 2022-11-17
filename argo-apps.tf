084bfa5345cf8bbcc2debe6a7e06fb2638607768
fortranhub <113641358+fortranhub@users.noreply.github.com>
2022-11-17 15:04:12 +0400

    Update argo-apps.tf

--- argo-apps.tf
+++ argo-apps.tf
@@ -4,4 +4,10 @@ resource "helm_release" "argocd_apps" {
     repository       = "https://argoproj.github.io/argo-helm"
     version          = "0.0.3"
     namespace        = "argo-cd"
+
+    values = [file("app-of-apps-values.yaml")]
+
+    depends_on = [
+      helm_release.argo-cd
+    ]
 }
